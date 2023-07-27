504b170b0c92f091ad94d6a206dd74b45b64f228
Artem Petrushenko <petrushenkoartemw@gmail.com>
2023-07-27 21:54:08 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/artempetrushenko/traffic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.artempetrushenko.traffic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:traffic/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
